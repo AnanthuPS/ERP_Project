9c7cce8143fcae541feb4ceccb4e19077861d587
ps <ananthups666@gmail.com>
2021-08-14 09:28:55 +0530

    Added Employee groups

--- ERP_system.py
+++ ERP_system.py
@@ -1,9 +1,10 @@
-#Mini project console ERP system.(Using Function)
+#Mini project console ERP system.(With Emp groups)
 
+employees = {}
+groups = {}
 
-employees={}
 def add_employee():
-	employee_id = input("\tenter Employee id ")
+	employee_id = input("\tEnter Employee id : ")
 	if employee_id not in employees.keys():
 		name = input("\tEnter name : ")
 		age = int(input("\tEnter age : "))
@@ -25,6 +26,7 @@ def add_employee():
 	else:
 			print("\tEmployee id  already Taken")
 
+
 def delete_employee():
 	eid = int(input("\tEnter employee id : "))
 	if eid not in employees.keys():
@@ -32,20 +34,23 @@ def delete_employee():
 	else:
 		del employees[employee_id]
 
+
 def search_employee():
-	name = input("\tEnter employee name : ")
+	name = input("\tEnter name : ")
 	found = False
 	for i in employees.values():
 		if i["name"] == name: 
-			print(f"\t{i['name']} | {i['age']} | {i['place']} | {i['gender']} | {i['previous_company']} | {i['salary']} ")
+			print(f"\t{i['name']} | {i['age']} | {i['place']} | {i['gender']} | {i['previous_company']} | {i['salary']}")
 			found = True
 			break
 		if found==False:
-			print("\tEmployee not in the list")
+			print("\tName not in the list")
+
 
 def display_employee():
 	for id,employee in employees.items():
-		print(f"\t{id} | {employee['name']} | {employee['age']} | {employee['place']} | {employee['gender']} | {employee['previous_company']} | {employee['salary']} ")
+		print(f"\t{id} | {employee['name']} | {employee['age']} | {employee['place']} | {employee['gender']} | {employee['previous_company']} | {employee['salary']}")
+
 
 def change_employee():
 	print("\t1.Change name")
@@ -66,6 +71,106 @@ def change_employee():
 		print("Invalid choice")
 
 
+def manage_all_group_menu():
+	print("\t1.Create Group")
+	print("\t2.Display Groups")
+	print("\t3.Manage Group(Particular)")
+	print("\t4.Delete Group")
+	print("\t5.Exit")
+
+def manage_all_groups():
+	while True:
+		manage_all_group_menu()
+		ch = int(input("\tEnter your choice : "))
+		if ch == 1:
+			#Create group
+			create_group()
+		elif ch == 2:
+			#display groups
+			display_groups()
+		elif ch == 3:
+			#Manage group(Particular)
+			manage_group()
+		elif ch == 4:
+			#Delete Group
+			delete_group()
+		elif ch == 5:
+			#exit
+			break
+		else:
+			print("\tInvalid choice !")
+
+
+def create_group():
+	group_name = input("\tEnter group name : ")
+	groups[group_name] = []
+
+
+def delete_group():
+	group_name = input("\tEnter group name to delete : ")
+	if group_name in groups.keys():
+		del groups[group_name]
+		print("\tDeleted the group")
+	else:
+		print("\tWrong group name")
+
+
+def display_groups():
+	for key,value in groups.items(): #key is group_name,value is list of student serial no
+		name_string = ""
+		for i in value:
+			name_string = name_string +"|"+employees[i]["name"]
+		print(f"{key} => {name_string}")
+
+
+def manage_group_menu():
+	print("\t\t1.Add Member")
+	print("\t\t2.Delete Member")
+	print("\t\t3.List Members")
+	print("\t\t4.Exit")
+
+
+def manage_group():
+	while True:
+		group_name = input("\t\tEnter group name : ")
+		manage_group_menu()
+		ch = int(input("\t\t Enter your Choice : "))
+		if ch == 1:
+			#Add member
+			add_member(group_name)
+		elif ch == 2:
+			#Delete member
+			delete_member(group_name)
+		elif ch == 3:
+			#List member
+			list_member(group_name)
+		elif ch == 4:
+			break	
+		else:
+			print("\tInvalid choice")
+
+def add_member(group_name):
+	display_employee()
+	serial_no = input("\t\tEnter the id of employee : ")
+	if serial_no in employees.keys():
+		groups[group_name].append(serial_no)
+	else:
+		print("\t\tWrong serial No.")
+
+def list_member(group_name):
+	name_string=""
+	for i in groups[group_name]:
+		name_string = name_string +"|"+i+"."+employees[i]["name"]
+	print(f"{name_string}")
+
+
+def delete_member(group_name):
+	list_member(group_name)
+	serial_no = input("\t\tEnter Emp id for deletion : ")
+	if serial_no in groups[group_name]:
+		groups[group_name].remove(serial_no)
+	else:
+		print("\t\tWrong serial No.")	
 
 
 while True:
@@ -74,7 +179,8 @@ while True:
 	print("3.Search employee")
 	print("4.Display  employee")
 	print("5.Change employee details")
-	print("6.Quit")
+	print("6.Manage all groups")
+	print("7.Quit")
 	ch = int(input("Enter your choice : "))
 	if ch == 1:
 	#adding employee
@@ -93,6 +199,9 @@ while True:
 	#change employee details
 		change_employee()
 	elif ch == 6:
+		#Manage	all groups
+		manage_all_groups() 
+	elif ch == 7:
 		break;
 	else:
 		print("Invalid Choice")
