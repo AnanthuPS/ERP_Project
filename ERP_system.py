6e611bf4ee47c9ef5fc6bcaa9ffa590abbc85c35
ps <ananthups666@gmail.com>
2021-08-14 09:24:20 +0530

    Added functions

--- ERP_system.py
+++ ERP_system.py
@@ -1,110 +1,101 @@
-print("ERP System using Dictionary")
-print("---------------------------")
+#Mini project console ERP system.(Using Function)
 
-emp = {}
+
+employees={}
+def add_employee():
+	employee_id = input("\tenter Employee id ")
+	if employee_id not in employees.keys():
+		name = input("\tEnter name : ")
+		age = int(input("\tEnter age : "))
+		gender = input("\tEnter the gender : ")
+		place = input("\tEnter place : ")
+		salary = int(input("\tEnter salary : "))
+		previous_company = input("\tEnter your previous company : ")
+		joining_date = input("\tEnter joining date : ")
+		temp ={
+			"name":name,
+			"age":age,
+			"gender":gender,
+			"place":place,
+			"salary":salary,
+			"previous_company":previous_company,
+			"joining_date":joining_date,
+			}
+		employees[employee_id] = temp
+	else:
+			print("\tEmployee id  already Taken")
+
+def delete_employee():
+	eid = int(input("\tEnter employee id : "))
+	if eid not in employees.keys():
+		print("\tWrong Employee id")
+	else:
+		del employees[employee_id]
+
+def search_employee():
+	name = input("\tEnter employee name : ")
+	found = False
+	for i in employees.values():
+		if i["name"] == name: 
+			print(f"\t{i['name']} | {i['age']} | {i['place']} | {i['gender']} | {i['previous_company']} | {i['salary']} ")
+			found = True
+			break
+		if found==False:
+			print("\tEmployee not in the list")
+
+def display_employee():
+	for id,employee in employees.items():
+		print(f"\t{id} | {employee['name']} | {employee['age']} | {employee['place']} | {employee['gender']} | {employee['previous_company']} | {employee['salary']} ")
+
+def change_employee():
+	print("\t1.Change name")
+	print("\t2.Change age")
+	print("\t3.Change gender")
+	print("\t4.Change salary")
+	cho =int(input("\tEnter your choice : "))
+	employee_id = input("\tEnter employee id : ")
+	if cho == 1:
+		employees[employee_id]['name'] = input("\tEnter new name : ") 
+	elif cho == 2:
+		employees[employee_id]['age'] = int(input("\tEnter new age : "))
+	elif cho == 3:
+		employees[employee_id]['gender'] = input("\tEnter gender : ")
+	elif cho == 4:
+		employees[employee_id]['salary'] = int(input("\tEnter new salary : "))
+	else:
+		print("Invalid choice")
+
+
+
+
 while True:
-	print("1.Add Employee")
-	print("2.Delete Employee")
-	print("3.Search Employee")
-	print("4.Change Employee Data")
-	print("5.Display Employee")
-	print("6.Exit")
-
+	print("1.Add employee")
+	print("2.Delete employee ")
+	print("3.Search employee")
+	print("4.Display  employee")
+	print("5.Change employee details")
+	print("6.Quit")
 	ch = int(input("Enter your choice : "))
 	if ch == 1:
-		#Add Employee
-		serial_no = int(input("\tEnter the serial number : "))
-
-		if serial_no not in emp.keys():
-			emp_id = int(input("\tEnter the employee ID : "))
-			name = input("\tEnter name : ")
-			age = int(input("\tEnter the age : "))
-			gender = input("\tEnter the gender : ")
-			place = input("\tEnter the place : ")
-			salary = int(input("\tEnter the salary : "))
-			prev_cmp = input("\tEnter the name of previous company : ")
-			date = input("\tEnter the joining date : ")
-
-			temp = {
-				"emp_id":emp_id,
-				"name":name,
-				"age":age,
-				"gender":gender,
-				"place":place,
-				"salary":salary,
-				"prev_cmp":prev_cmp,
-				"date":date
-				}
-
-			emp[serial_no] = temp
-
-		else:
-			print("\tSerial number already in the dictionary!!!")
-
+	#adding employee
+		add_employee()
+			
 	elif ch == 2:
-		#Delete Employee
-		print(emp)
-		print("\tChoose serial num of employee for deletion ")
-		serial = int(input("\tEnter serial num to delete : "))
-		if serial in emp.keys():
-			del emp[serial]
-		else:
-			print(f"\t{serial} not in the dictionary")
-
+	#delete
+		delete_employee()
 	elif ch == 3:
-		#Search Employee
-		name = input("\tEnter name you want to search : ")
-		found = False
-		for i in emp.values():
-			if i["name"] == name:
-				print(f"\t{i['emp_id']} | {i['name']} | {i['age']} | {i['gender']} | {i['place']} | {i['salary']} | {i['prev_cmp']} | {i['date']}")
-				found = True
-				break
-		if found == False:
-			print("\tData not found")
-
-
+	#search
+		search_employee()
 	elif ch == 4:
-                #Change a Employee data
-		print("1  .Change name")
-		print("2  .change age")
-		print("3  .Change gender")
-		print("4  .change salary")
-		choice = int(input("/tenter your choice : "))
-		if choice == 1:
-			serial_no = int(input("\tEnter serial number :"))
-			if serial_no not in emp.keys():
-				print("\twrong employee no")
-			else:
-				emp[serial_no]['name'] = input("\tEnter new name")  
-		elif choice == 2:
-			serial_no = int(input("\tEnter serial number :"))
-			if serial_no not in emp.keys():
-				print("\twrong employee no")
-			else:
-				emp[serial_no]['age'] = input("\tEnter new age")  
-		elif choice == 3:
-			serial_no = int(input("\tEnter serial number :"))
-			if serial_no not in emp.keys():
-				print("\twrong employee no")
-			else:
-				emp[serial_no]['gender'] = input("\tEnter new gender")
-		elif choice == 4:
-			serial_no = int(input("\tEnter serial number :"))
-			if serial_no not in emp.keys():
-				print("\twrong employee no")
-			else:
-				emp[serial_no]['salary'] = input("\tEnter new salary")
-
-
-	elif ch == 5:
-		#Display Employee
-		for serial,empl in emp.items():
-			print(f"\t{serial} | {empl['emp_id']} | {serial} | {empl['name']} | {empl['age']} | {empl['gender']} | {empl['place']} | {empl['salary']} | {empl['prev_cmp']} | | {empl['date']}")
-
-
+	#display employee
+		display_employee()
+	elif ch== 5:
+	#change employee details
+		change_employee()
 	elif ch == 6:
-		break
-
+		break;
 	else:
 		print("Invalid Choice")
+
+
+
